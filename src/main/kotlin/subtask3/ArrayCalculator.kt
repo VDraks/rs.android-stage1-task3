d4ab88c4f79b9ea3a0ff4e9e9a2b4f30f812bcd5
VDraks <kytinvasya@gmail.com>
2020-05-06 22:00:49 +0300

    feat: implement ArrayCalculator

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -1,9 +1,23 @@
 package subtask3
 
+import kotlin.math.abs
+
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        val intItems = itemsFromArray.filterIsInstance<Int>()
+        if (intItems.isEmpty()) return 0
+        if (intItems.size <= numberOfItems) return intItems.fold(1, { acc, i -> acc * i })
+
+        val absList = intItems.sortedWith(compareByDescending { abs(it) })
+
+        val firstN = absList.take(numberOfItems).toMutableList()
+        val negativeCount = firstN.filter { it < 0 }.size
+        if (negativeCount % 2 == 0) return firstN.fold(1, { acc, i -> acc * i })
+
+        firstN.remove(firstN.last { it < 0 })
+        firstN.add(absList.takeLast(absList.size - numberOfItems).first { i -> i > 0 })
+
+        return firstN.fold(1, { acc, i -> acc * i })
     }
 }
