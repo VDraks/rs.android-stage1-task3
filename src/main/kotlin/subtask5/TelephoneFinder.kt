f683e546831c186749a44818d4eda581a8190919
VDraks <kytinvasya@gmail.com>
2020-05-06 22:59:31 +0300

    feat: implement TelephoneFinder

--- src/main/kotlin/subtask5/TelephoneFinder.kt
+++ src/main/kotlin/subtask5/TelephoneFinder.kt
@@ -1,11 +1,33 @@
 package subtask5
 
-import kotlin.reflect.KClass
-
 class TelephoneFinder {
 
-    // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        if (number.toInt() < 0) return null
+
+        val result = mutableListOf<String>()
+
+        number.forEachIndexed { index, digit ->
+            fun replaceChars(neighbors: List<Char>) {
+                for (element in neighbors) {
+                    result.add(number.replace(number[index], element))
+                }
+            }
+
+            when (digit) {
+                '0' -> replaceChars(listOf('8'))
+                '1' -> replaceChars(listOf('2', '4'))
+                '2' -> replaceChars(listOf('1', '3', '5'))
+                '3' -> replaceChars(listOf('2', '6'))
+                '4' -> replaceChars(listOf('1', '5', '7'))
+                '5' -> replaceChars(listOf('2', '4', '6', '8'))
+                '6' -> replaceChars(listOf('3', '5', '9'))
+                '7' -> replaceChars(listOf('4', '8'))
+                '8' -> replaceChars(listOf('5', '7', '9', '0'))
+                '9' -> replaceChars(listOf('6', '8'))
+            }
+        }
+
+        return result.toTypedArray()
     }
 }
